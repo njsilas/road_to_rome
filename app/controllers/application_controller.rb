88ad42b23d460961b942e12d80f79a97ebb72b58
Nicholas Silas <njsilas@gmail.com>
2021-01-18 15:02:10 -0500

    added helpers for app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+    end
+    def logged_in?
+        current_user.id != nil
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+has_secure_password
 has_many :gears
 has_many :booked_trips
 has_many :flights, through: :booked_trips
