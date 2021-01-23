852c48d0eab88fa7575b2b3087127039a956f7cb
Nicholas Silas <njsilas@gmail.com>
2021-01-22 19:00:49 -0500

    working validation for user log in, creating helper for view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,15 @@
 class UsersController < ApplicationController
- def new
-@user = User.new
- end
+   def new
+    @user = User.new
+   end
  def create
-    
-    if params[:email] == "" || params[:name] == "" || params[:password] == ""
-        render 'new'
-    else
-        @user = User.create(user_params)
-        
-        session[:name] = params[:name]
-        redirect_to user_path(@user)
-    end
-    def show
-        @user = User.find_by_id(params[:id])
-    end
+    @user = User.create(user_params)
+        @user.doublecheck
  end
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+ 
  private
  def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ has_secure_password
 has_many :booked_trips
 has_many :flights, through: :booked_trips
 has_many :destinations
-validates :email, presence: true, message: "Email Required"
-validates :email, uniqueness: true, message: "Account Already Exists"
-validates :password, presence: true, message: "Password required"
+validates :email, 
+presence: { is: true, message: "Email Required" }, 
+format: { with: /@/, message: "Needs to be valid email" }, 
+uniqueness: { is: true, message: "Account Already Exists" }
+validates :password, presence: { is: true, message: "Password required" }
+  def self.doublecheck
+    if self.valid?
+        self.save
+        redirect_to users_path(self)
+    else
+        render :new
+    end
+  end
 end
