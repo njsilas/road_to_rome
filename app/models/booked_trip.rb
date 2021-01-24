73fe433de749fad4d73dff4eadeebf26ee5c9fcd
Nicholas Silas <njsilas@gmail.com>
2021-01-24 17:06:20 -0500

    index working, scope not

--- app/models/booked_trip.rb
+++ app/models/booked_trip.rb
@@ -4,11 +4,14 @@ belongs_to :flight
 belongs_to :user
 monetize :savings_cents, as: "savings"
 accepts_nested_attributes_for :flight
-scope :mytripz, -> {where('user_id = ?', current_user.id)}
+scope :mytripz, -> { where("user_id = ?", current_user.id) }
     def dest_price
       self.flight.destination.lodging_price
     end
     def fly_price
       self.flight.ticket_price
+    end
+    def unscoped
+        @booked_trips.where("user_id = ?", current_user.id)
     end
 end
