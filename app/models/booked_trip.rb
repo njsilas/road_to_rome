63f98d5c64313ff68eed3760faec8a77997c026c
Nicholas Silas <njsilas@gmail.com>
2021-01-24 14:54:43 -0500

    tweaking scope at the moment

--- app/models/booked_trip.rb
+++ app/models/booked_trip.rb
@@ -1,10 +1,10 @@
 class BookedTrip < ApplicationRecord
-    include MoneyRails::ActionViewExtension
+    #include MoneyRails::ActionViewExtension
 belongs_to :flight
 belongs_to :user
 monetize :savings_cents, as: "savings"
 accepts_nested_attributes_for :flight
-scope :mytripz, -> {where('user_id = ?', self.user_id)}
+scope :mytripz, -> {where('user_id = ?', current_user.id)}
     def dest_price
       self.flight.destination.lodging_price
     end
