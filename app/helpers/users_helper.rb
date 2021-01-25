154ad81e2f58aab249fd533f6ddf9a15de3bc3c0
Nicholas Silas <njsilas@gmail.com>
2021-01-25 02:16:00 -0500

    ready for demo, need to clean up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+    def trips_display
+       if current_user.destinations.exists?
+            current_user.destinations.each do |destination|
+              destination.flights
+              if destination.flights.upcoming.empty?
+                "you have no flights comping this month"
+              else
+              "You've got a trip coming up!"
+            end
+        
+        end
+    end
+    
+    end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,11 +5,11 @@ has_many :users, through: :booked_trips
 monetize :ticket_price_cents
 accepts_nested_attributes_for :destination    
 validate :invalid_arrival
-
+scope :upcoming, -> {where('arrival = ?', Date.today.mon )}
   def invalid_arrival
       if arrival.present? && arrival < Date.today
         errors.add(:arrival, "can't be in the past")
       end
   end 
-
+  
 end
