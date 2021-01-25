46d45dd05951ae1b8dbf29d7da31ec9be026772b
Nicholas Silas <njsilas@gmail.com>
2021-01-25 16:11:36 -0500

    removed some helpers, still working. onto my show page

--- app/models/booked_trip.rb
+++ app/models/booked_trip.rb
@@ -5,12 +5,7 @@ belongs_to :user
 monetize :savings_cents, as: "savings"
 accepts_nested_attributes_for :flight
 scope :mytrips, ->(user){where("user_id = ?", user.id)}
-    def dest_price
-      self.flight.destination.lodging_price
-    end
-    def fly_price
-      self.flight.ticket_price
-    end
+   
     def unscoped
         self.where("user_id = ?", current_user.id)
     end
