638973b31db48858d6c74f04cc855d0bc15aae4d
Nicholas Silas <njsilas@gmail.com>
2021-01-25 20:59:15 -0500

    added new scope, implimenting to clean up show page

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,7 +5,8 @@ has_many :users, through: :booked_trips
 monetize :ticket_price_cents
 accepts_nested_attributes_for :destination    
 validate :invalid_arrival
-scope :upcoming, -> {where('arrival = ?', Date.today.mon )}
+scope :thismonth, -> {where('arrival = ?', Date.today.mon )}
+scope :distant, -> {where('arrival != ?', Date.today.mon )}
   def invalid_arrival
       if arrival.present? && arrival < Date.today
         errors.add(:arrival, "can't be in the past")
