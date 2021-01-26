1a8ad0ef2ed6e5feaa7daf85260b93159e62a3f9
Nicholas Silas <njsilas@gmail.com>
2021-01-26 01:42:25 -0500

    completed two extra scopes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 module UsersHelper
-
-  
+  def totlodgenum
+    humanized_money_with_symbol(current_user.destinations.totlodge) 
+  end
+  def totflightnum
+    humanized_money_with_symbol(current_user.flights.totflight) 
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,7 +7,8 @@ accepts_nested_attributes_for :destination
 validate :invalid_arrival
 scope :thismonth, -> {where('arrival = ?', Date.today.mon )}
 scope :distant, -> {where('arrival != ?', Date.today.mon )}
-  def invalid_arrival
+scope :totflight, -> { sum(:ticket_price_cents) }
+def invalid_arrival
       if arrival.present? && arrival < Date.today
         errors.add(:arrival, "can't be in the past")
       end
