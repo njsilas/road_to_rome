dd412d319cf7070516e6514df68db47731b8b711
Nicholas Silas <njsilas@gmail.com>
2021-01-27 02:00:07 -0500

    testing validation for only positive number

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,4 +4,5 @@ class Destination < ApplicationRecord
     has_many :booked_trips, through: :flights
    monetize :lodging_price_cents
     scope :totlodge, -> { sum(:lodging_price_cents) }
+    validate :lodging_price_cents, numericality: { greater_than: 0 }
 end
