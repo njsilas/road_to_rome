d46ca4875889712c0c2a7ef7317012435bbb722a
Nicholas Silas <njsilas@gmail.com>
2021-01-27 14:10:23 -0500

    adding validation for invalid departures

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,14 +5,21 @@ has_many :users, through: :booked_trips
 monetize :ticket_price_cents
 accepts_nested_attributes_for :destination    
 validates :ticket_price, numericality:{ greater_than: 0 }
+validates :arrival, :departure, presence: true
 validate :invalid_arrival
+validate :invalid_departure
 scope :thismonth, -> {where('arrival = ?', Date.today.mon )}
 scope :distant, -> {where('arrival != ?', Date.today.mon )}
 scope :totflight, -> { sum(:ticket_price_cents) }
+private
 def invalid_arrival
       if arrival.present? && arrival < Date.today
         errors.add(:arrival, "can't be in the past")
       end
   end 
-  
+  def invalid_departure
+    if departure.present? && arrival.present? && departure < arrival
+      errors.add(:departure, "must be past arrival date")
+    end
+  end
 end
