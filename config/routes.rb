2b001220c046938d892668f14204c448c17902cf
Nicholas Silas <njsilas@gmail.com>
2021-02-02 12:36:30 -0500

    added 404 errormessage

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   end
 
   
-  
+  match '*unmatched', to: 'application#route_not_found', via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
