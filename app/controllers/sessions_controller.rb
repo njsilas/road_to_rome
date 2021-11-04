6ebc0eea87a2cfd9d8f3b2fd783753e936dd989b
Nicholas Silas <njsilas@gmail.com>
2021-11-04 16:56:45 -0400

    cleaning up messy code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     end
     def create
         @user = User.find_by(email: params["email"])
-        
           if @user && @user.authenticate(params["password"])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
