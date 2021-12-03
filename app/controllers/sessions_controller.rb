66107f1d4c713e141a64df7395ff9f48c8f4b4b6
Nicholas Silas <njsilas@gmail.com>
2021-12-02 19:03:05 -0500

    fixing helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
           else
-        
             redirect_to '/login', alert: 'Please try again'
           end
       end
